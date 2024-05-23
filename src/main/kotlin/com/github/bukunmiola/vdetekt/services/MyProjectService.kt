0760169406105851ea5d84aade9845bc2d3bfb43
GitHub Action <action@github.com>
2024-05-23 08:56:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/bukunmiola/vdetekt/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bukunmiola/vdetekt/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.bukunmiola.vdetekt.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bukunmiola.vdetekt.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
